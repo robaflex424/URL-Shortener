931176ac0ebd046eda1ee32047e01a0c3bbe614b
Roberti Pichkhnarashvili <robaflex77@gmail.com>
2026-09-01 15:00:38 +0400

    Added endpoint which redirects user to original url

--- routers/url.py
+++ routers/url.py
@@ -1,19 +1,26 @@
+import datetime
+from time import timezone
 from typing import Annotated
 from sqlalchemy.orm import Session 
 from database.database import get_db
 from schemas.url import URLCreate, URLResponse 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from utils.utils import generate_short_code
 from models.url import Url
+from fastapi.responses import RedirectResponse
 
-router = APIRouter(
+url_router = APIRouter(
   prefix="/urls",
   tags=["urls"]
 )
 
+redirect_router = APIRouter(
+  tags=["redirect"]
+)
+
 db_dependency = Annotated[Session, Depends(get_db)]
 
-@router.post("", response_model=URLResponse)
+@url_router.post("", response_model=URLResponse)
 async def create_url(db: db_dependency, url: URLCreate):
   short_code = generate_short_code()
 
@@ -31,3 +38,34 @@ async def create_url(db: db_dependency, url: URLCreate):
   db.refresh(new_url)
 
   return new_url
+
+@redirect_router.get("{short_code}")
+async def redirect_user_to_url(short_code: str, db: db_dependency):
+  url = db.query(Url).filter(Url.short_code == short_code).first()
+
+  if url is None:
+    raise HTTPException(
+      status_code=404,
+      detail="URL not found"
+    )
+  
+  if url.is_active is False: 
+    raise HTTPException(
+      status_code=404,
+      detail="URL is inactive"
+    )
+  
+  if url.expires_at is not None and url.expires_at <= datetime.now(timezone.utc):
+    raise HTTPException(
+      status_code=404,
+      detail="URL is expired"
+    )
+  
+  url.click_count += 1
+
+  db.commit() 
+
+  return RedirectResponse(
+    url=url.original_url,
+    status_code=307
+  )
