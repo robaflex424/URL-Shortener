67435d9bd8124705f44a0fc59286ab21d3f29b32
Roberti Pichkhnarashvili <robaflex77@gmail.com>
2026-09-01 14:40:25 +0400

    added /urls endpoint, where u can create shortened url

--- routers/url.py
+++ routers/url.py
@@ -12,3 +12,22 @@ router = APIRouter(
 )
 
 db_dependency = Annotated[Session, Depends(get_db)]
+
+@router.post("", response_model=URLResponse)
+async def create_url(db: db_dependency, url: URLCreate):
+  short_code = generate_short_code()
+
+  while db.query(Url).filter(Url.short_code == short_code).first() is not None: 
+    short_code = generate_short_code()
+  
+  new_url = Url(
+    original_url = str(url.original_url),
+    short_code  = short_code,
+    expires_at = url.expires_at
+  )
+
+  db.add(new_url)
+  db.commit()
+  db.refresh(new_url)
+
+  return new_url
